c9a12eb89de3e7262db6a4ec76703862704c61a4
MinGOODdev <mingood92@gmail.com>
2018-08-31 00:13:36 +0900

    Test: Rest(Guest, Product)ControllerTest
    - JUnit
    - MockMvc

---
+++ src/test/java/com/kakao/ix/controller/RestGuestControllerTest.java
@@ -0,0 +1,67 @@
+package com.kakao.ix.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakao.ix.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RestGuestControllerTest {
+
+  @Autowired
+  private WebApplicationContext wac;
+
+  private MockMvc mockMvc;
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf-8"));
+
+  @Before
+  public void init() throws Exception {
+    this.mockMvc = webAppContextSetup(wac).build();
+  }
+
+  @Test
+  public void 회원가입() throws Exception {
+    User user = User.builder()
+            .login("moonilly")
+            .password("111111")
+            .build();
+
+    MvcResult mvcResult = mockMvc.perform(post("/rest/register")
+            .contentType(contentType)
+            .content(asJsonString(user)))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andReturn();
+
+    log.info(mvcResult.getResponse().getContentAsString());
+  }
+
+  public static String asJsonString(Object obj) {
+    try {
+      final ObjectMapper objectMapper = new ObjectMapper();
+      final String jsonContent = objectMapper.writeValueAsString(obj);
+      return jsonContent;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
---
+++ src/test/java/com/kakao/ix/controller/RestProductControllerTest.java
@@ -0,0 +1,59 @@
+package com.kakao.ix.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RestProductControllerTest {
+
+  @Autowired
+  private WebApplicationContext wac;
+
+  private MockMvc mockMvc;
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf-8"));
+
+  @Before
+  public void init() throws Exception {
+    this.mockMvc = webAppContextSetup(wac).build();
+  }
+
+  @Test
+  public void 상품_목록_전체_조회() throws Exception {
+    MvcResult mvcResult = mockMvc.perform(get("/rest/kakao/products"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andDo(print())
+            .andReturn();
+    log.info(mvcResult.getResponse().getContentAsString());
+  }
+
+  @Test
+  public void 상품_개별_조회() throws Exception {
+    MvcResult mvcResult = mockMvc.perform(get("/rest/kakao/product/2"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andDo(print())
+            .andReturn();
+    log.info(mvcResult.getResponse().getContentAsString());
+  }
+
+}
