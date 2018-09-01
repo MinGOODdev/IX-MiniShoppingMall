0dde6fd1dcc60f0a812cd85a28240fa618e96530
MinGOODdev <mingood92@gmail.com>
2018-09-02 04:07:26 +0900

    Feat: Product Function
    해당 상품 장바구니에 담기

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # KakaoIX - Mini Shopping Mall
 ### 2018 카카오IX 8월 (신입)경력 공채
 
+## API
+* [비회원_API](https://github.com/MinGOODdev/KakaoIX-MiniShoppingMall/wiki/1.-RestGuestController)
+* [상품_API](https://github.com/MinGOODdev/KakaoIX-MiniShoppingMall/wiki/2.-RestProductController)
+* [장바구니_API](https://github.com/MinGOODdev/KakaoIX-MiniShoppingMall/wiki/3.-RestCartController)
+* [구매내역_API](https://github.com/MinGOODdev/KakaoIX-MiniShoppingMall/wiki/4.-RestOrderController)
+
 ## Use
 * Spring Boot
 * Security
--- src/main/java/com/kakao/ix/controller/ProductController.java
+++ src/main/java/com/kakao/ix/controller/ProductController.java
@@ -1,13 +1,14 @@
 package com.kakao.ix.controller;
 
 import com.kakao.ix.domain.Product;
+import com.kakao.ix.domain.User;
+import com.kakao.ix.service.CartService;
 import com.kakao.ix.service.ProductService;
+import com.kakao.ix.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +18,10 @@ public class ProductController {
 
   @Autowired
   private ProductService productService;
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private CartService cartService;
 
   /**
    * 상품 목록 전체 조회
@@ -42,6 +47,20 @@ public class ProductController {
     Product product = productService.findById(id);
     model.addAttribute("product", product);
     return "user/productDetail";
+  }
+
+  /**
+   * 해당 상품 장바구니에 담기
+   * @param productId
+   * @param number
+   * @return
+   */
+  @PostMapping("product/cart")
+  public String productInCart(@RequestParam("productId") int productId,
+                              @RequestParam("number") int number) {
+    User currentUser = userService.currentLoginUser();
+    cartService.insertProduct(currentUser.getId(), productId, number);
+    return "redirect:/kakao/cart";
   }
 
 }
