482b526151487acc1cd021dd82e5973599bdd265
MinGOODdev <mingood92@gmail.com>
2018-09-02 22:29:37 +0900

    Feat: CorsFilter

---
+++ src/main/java/com/kakao/ix/config/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.kakao.ix.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest,
+                       ServletResponse servletResponse,
+                       FilterChain filterChain) throws IOException, ServletException {
+    HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+    httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With");
+    httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+    httpServletResponse.setHeader("Access-Control-Expose-Headers", "Location");
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+
+}
